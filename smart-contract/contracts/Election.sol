61fca3bb298864a32a652ac5d5d6ee00bf2246b4
Pelumi527 <fatolupelumi@gmail.com>
2022-04-21 23:22:26 +0100

    merged

--- smart-contract/contracts/Election.sol
+++ smart-contract/contracts/Election.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import '@openzeppelin/contracts/security/Pausable.sol';
+import './ElectionAccessControl.sol';
 
-contract Election is Pausable{
+contract Election is Pausable, ElectionAccessControl{
 
 
     string position;
@@ -17,29 +18,52 @@ contract Election is Pausable{
     error AlreadyVoted();
     error ResultNotYetRelease();
 
+    struct Candidates{
+        address candidatesAddress;
+        uint voteCount;
+    }
+
     mapping(address => bool) voterStatus;
+    mapping(address => Candidates) candidates;
     mapping(address => uint) voteCount;
-    uint[] results;
+    Candidates[] results;
+
+    
+
+    
 
 
-    constructor(string memory _position, uint _noOfParticipants, address[] memory _participantsAddress){
+    constructor(string memory _position, uint _noOfParticipants, address[] memory _participantsAddress) ElectionAccessControl(msg.sender){
         if(_noOfParticipants != _participantsAddress.length) revert NoOfParticatantNotMatchingParticipateId();
         position = _position;
         noOfpartcipate = _noOfParticipants;
         participateAddress = _participantsAddress;
+        for(uint i=0; i < _participantsAddress.length;i++){
+           Candidates storage _candidates = candidates[_participantsAddress[i]];
+           _candidates.candidatesAddress = _participantsAddress[i];
+        }
     }
 
 
     function setupTeachers(address[] memory _teacher) public returns(bool){
-        // to do 
+        for(uint i = 0; i < _teacher.length; i++){
+            _grantRole(TEACHER_ROLE, _teacher[i]);
+        }
+        return true;
     }
 
     function registerStudent(address[] memory _student) public returns(bool){
-
+        for(uint i = 0; i < _student.length; i++){
+            _grantRole(STUDENT_ROLE, _student[i]);
+        }
+        return true;
     }
 
     function setupBOD(address[] memory _Bod) public returns(bool){
-
+        for(uint i = 0; i < _Bod.length; i < i++){
+            _grantRole(DIRECTOR_ROLE, _Bod[i]);
+        }
+        return true;
     }
 
     function vote(address _participantsAddress) public whenNotPaused returns(bool){
@@ -50,9 +74,11 @@ contract Election is Pausable{
         return true;
     }
 
-    function compileResult() public returns(uint[] memory){
+    function compileResult() public returns(Candidates[] memory){
         for(uint i = 0; i < participateAddress.length; i++){
-            results.push(voteCount[participateAddress[i]]);
+            Candidates storage _candidates = candidates[participateAddress[i]];
+            _candidates.voteCount = voteCount[participateAddress[i]];
+            results.push(_candidates);
         }
         return results;
     }
@@ -62,7 +88,7 @@ contract Election is Pausable{
         return true;
     }
 
-    function result() public view returns(uint[] memory){
+    function result() public view returns(Candidates[] memory){
        // if(resultStatus == false) revert ResultNotYetRelease();
         return results;
     }
